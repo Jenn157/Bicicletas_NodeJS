68024a05af1e3adc94ce82f6d68a8b3698996bca
Jennifer Acuña <jdacunaa@udistrital.edu.co>
2024-05-01 19:06:40 -0500

    Inicio express

--- Proyecto_NodeJS/app.js
+++ Proyecto_NodeJS/app.js
@@ -1,15 +1,21 @@
+// Realizado por:
+// -Santiago Andres Rodriguez
+// -Jennifer Acuña Amado
+
+//Importacion de todos los modulos 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Importar los archivos js index y users
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// Definir el motor de las vistas 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -19,15 +25,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//En la ruta raiz va a ubicar el modulo indexRouter, igual con user se define la ruta
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Validacion de error 404 
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Validacion de posibles errores mostrando sus codigos 
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -38,4 +45,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//Exportar app 
 module.exports = app;
--- Proyecto_NodeJS/routes/index.js
+++ Proyecto_NodeJS/routes/index.js
@@ -1,9 +1,15 @@
+// Realizado por:
+// -Santiago Andres Rodriguez
+// -Jennifer Acuña Amado
+
+//Importar modulo express
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Metodo que maneja las rutas, va a generar el archivo html, llama la vista index y le envia el atributo title */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Exporta la variable router
 module.exports = router;
